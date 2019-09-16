638aa6d315469504932823f35fee798e9dd252cb
hemie143 <hemie143@gmail.com>
2019-09-16 17:47:15 +0200

    page 049

---
+++ flask-hello-world/app48.py
@@ -0,0 +1,49 @@
+# ---- Flask Hello Word ---- #
+
+# Import the Flask class from the flask package
+from flask import Flask
+
+# Create the application object
+app = Flask(__name__)
+
+# Error handling
+app.config["DEBUG"] = True
+
+# Use the decorator pattern to link the view function to a url
+@app.route("/")
+@app.route("/hello")
+
+# define the view using a function which returns a string
+def hello_world():
+    return "Hello, World!?!?!?!"
+
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+    print(value + 1)
+    return "correct"
+
+@app.route("/float/<float:value>")
+def float_type(value):
+    print(value + 1)
+    return "correct"
+
+@app.route("/path/<path:value>")
+def path_type(value):
+    print(value)
+    return "correct"
+
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "michael":
+        return "Hello, {}".format(name)
+    else:
+        return "Not Found", 404
+
+# Start the development server using the run() method
+if __name__ == "__main__":
+    app.run()
+
