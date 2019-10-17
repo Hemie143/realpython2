2fba455a1d558fb11d1c303831a3f2d75d2593d4
hemie143 <hemie143@gmail.com>
2019-10-17 13:17:12 +0200

    page 198

---
+++ chp11_flasktaskr_part04/project/db_create.py
@@ -0,0 +1,12 @@
+# project/db_create.py
+
+from views import db
+from models import Task
+from datetime import date
+
+db.create_all()
+
+# db.session.add(Task("Finish this tutorial", date(2016,  9, 22), 10, 1))
+# db.session.add(Task("Finish Real Python", date(2016,  10, 3), 10, 1))
+
+db.session.commit()
---
+++ chp11_flasktaskr_part04/project/run.py
@@ -0,0 +1,5 @@
+# import realpython2.chp09_flasktaskr_part03.views
+# from realpython2.chp09_flasktaskr_part03.views import app
+from views import app
+
+app.run(debug=True)
