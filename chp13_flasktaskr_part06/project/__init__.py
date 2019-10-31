b0561ac4080da35a13791903cb177d5bf1781ec3
hemie143 <hemie143@gmail.com>
2019-10-31 16:41:21 +0100

    page 245

--- chp13_flasktaskr_part06/project/__init__.py
+++ chp13_flasktaskr_part06/project/__init__.py
@@ -1,6 +1,6 @@
 # project/__init__.py
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
@@ -14,3 +14,7 @@ from chp13_flasktaskr_part06.project.tasks.views import tasks_blueprint
 
 app.register_blueprint(users_blueprint)
 app.register_blueprint(tasks_blueprint)
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
