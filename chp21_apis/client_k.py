4a41eb0e6628c385aba6d7e2c40f7d1a8695a781
hemie143 <hemie143@gmail.com>
2019-12-03 07:10:35 +0100

    page 365

---
+++ chp21_apis/client_k.py
@@ -0,0 +1,14 @@
+import tweepy
+
+consumer_key = "<get_your_own>"
+consumer_secret = "<get_your_own>"
+access_token = "<get_your_own>"
+access_secret = "<get_your_own>"
+
+auth = tweepy.auth.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+api = tweepy.API(auth)
+
+tweets = api.search(q='#python')
+for t in tweets:
+    print(t.created_at, t.text, "\n")
